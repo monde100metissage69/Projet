d8c08a31539605a9c86c8bbf2ce3b0f72ba9a6da
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 13:30:09 +0200

    Create mouvement1.h

---
+++ mouvement1.h
@@ -0,0 +1,9 @@
+void up(int *x, int *y, int width, struct Case T[][width])
+{
+    if (*x - 1 >= 0 && T[*x - 1][*y].down == 0 && T[*x][*y].Up == 0)
+    {
+        *x = *x - 1;
+        return up(x, y, width, T);
+    }
+    printf("x = %d, y = %d\n", *x, *y);
+}
