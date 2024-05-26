b3cc8e2e9b1461995e08e7f24480b5e7f59f7621
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 22:44:46 +0200

    Update mouvement.h

--- mouvement.h
+++ mouvement.h
@@ -3,6 +3,10 @@
 
 #include <stlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h> 
+#include <limits.h>
+#include <ctype.h>
 void up(int *x, int *y, int width, struct Cell grid[][width]);
 void down(int *x, int *y, int width, int height, struct Cell grid[][width]);
 void left(int *x, int *y, int height, struct Cell grid[][height]);
