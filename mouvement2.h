7e397bed00da42763fd501a5f690529a02e9a02b
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 14:29:45 +0200

    Delete mouvement2.h

--- mouvement2.h
+++
@@ -1,7 +0,0 @@
-void down(int *x, int *y, int width, int height, struct Cell grid[][width]) {
-    if (*x + 1 < height && grid[*x + 1][*y].top == 0 && grid[*x][*y].bottom == 0) {
-        *x = *x + 1;
-        return down(x, y, width, height, grid);
-    }
-    printf("x = %d, y = %d\n", *x, *y);
-}
