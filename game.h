18d43313c295a29d96347766bf63297b2cd35e5d
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 22:45:08 +0200

    Update game.h

--- game.h
+++ game.h
@@ -2,6 +2,10 @@
 #define GAME_H
 #include <stdio.h>
 #include <stlib.h>
+#include <time.h>
+#include <unistd.h> 
+#include <limits.h>
+#include <ctype.h>
 void play_round(int num_players, int height, int width, struct Cell grid[][width], int robots[][2], int *scores, int *valid_targets, int num_valid_targets);
 #endif
  
