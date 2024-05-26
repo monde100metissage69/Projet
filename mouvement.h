4a461f1870dbe05bb63c000a7d831b527d3580e8
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 22:11:33 +0200

    Update mouvement.h

--- mouvement.h
+++ mouvement.h
@@ -4,7 +4,8 @@
 #include <stlib.h>
 
 void up(int *x, int *y, int width, struct Cell grid[][width]);
-
-
+void down(int *x, int *y, int width, int height, struct Cell grid[][width]);
+void left(int *x, int *y, int height, struct Cell grid[][height]);
+void right(int *x, int *y, int width, int height, struct Cell grid[][width]);
 
 #endif
