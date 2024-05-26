210bc85523c71f8a1cc98d97337d8592ad798040
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 14:30:16 +0200

    Delete mouvement4.h

--- mouvement4.h
+++
@@ -1,7 +0,0 @@
-void right(int *x, int *y, int width, int height, struct Cell grid[][width]) {
-    if (*y + 1 < width && grid[*x][*y + 1].left == 0 && grid[*x][*y].right == 0) {
-        *y = *y + 1;
-        return right(x, y, width, height, grid);
-    }
-    printf("x = %d, y = %d\n", *x, *y);
-}
