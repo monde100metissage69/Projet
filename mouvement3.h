5211e974a68083fb6395c58e41d8a1bb8a41b4c3
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 14:00:29 +0200

    Update mouvement3.h

--- mouvement3.h
+++ mouvement3.h
@@ -1,9 +1,7 @@
-void left(int *x, int *y, int length, struct Case T[][length])
-{
-    if (*y - 1 >= 0 && T[*x][*y - 1].right == 0 && T[*x][*y].left == 0)
-    {
+void left(int *x, int *y, int height, struct Cell grid[][height]) {
+    if (*y - 1 >= 0 && grid[*x][*y - 1].right == 0 && grid[*x][*y].left == 0) {
         *y = *y - 1;
-        return left(x, y, length, T);
+        return left(x, y, height, grid);
     }
     printf("x = %d, y = %d\n", *x, *y);
 }
