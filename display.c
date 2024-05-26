ce2998f2c1935e7317d26cc505704b8786e95f15
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 14:03:56 +0200

    Update display.c

--- display.c
+++ display.c
@@ -1,94 +1,63 @@
-void print_grid(int length, int width, struct Case T[][width], int robots[][2])
-{
-    for (int i = 0; i < length; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (i == 0)
-            {
-                printf(red "+----" stop);
-            }
-            else
-            {
-                if (j == 0)
-                {
-                    printf(red "+" stop);
-                    if (T[i][0].Up != 0)
-                    {
-                        printf(red "----" stop);
+void print_grid(int height, int width, struct Cell grid[][width], int robots[][2]) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (i == 0) {
+                printf(RED "+----" STOP);
+            } else {
+                if (j == 0) {
+                    printf(RED "+" STOP);
+                    if (grid[i][0].top != 0) {
+                        printf(RED "----" STOP);
+                    } else {
+                        printf(GRAY "----" STOP);
                     }
-                    else
-                    {
-                        printf(gray "----" stop);
-                    }
-                }
-                else
-                {
-                    if (T[i][j].Up != 0 || T[i - 1][j].down != 0)
-                    {
-                        printf(red "+" stop);
-                        printf(red "----" stop);
-                    }
-                    else
-                    {
-                        printf(gray "+" stop);
-                        printf(gray "----" stop);
+                } else {
+                    if (grid[i][j].top != 0 || grid[i - 1][j].bottom != 0) {
+                        printf(RED "+" STOP);
+                        printf(RED "----" STOP);
+                    } else {
+                        printf(GRAY "+" STOP);
+                        printf(GRAY "----" STOP);
                     }
                 }
             }
         }
-        printf(red "+\n" stop);
+        printf(RED "+\n" STOP);
 
-        for (int j = 0; j < width; j++)
-        {
-            int robot_present = 0;
-            for (int k = 0; k < 4; k++)
-            {
-                if (robots[k][0] == i && robots[k][1] == j)
-                {
-                    robot_present = 1;
+        for (int j = 0; j < width; j++) {
+            int robot_present = -1;
+            for (int k = 0; k < 4; k++) {
+                if (robots[k][0] == i && robots[k][1] == j) {
+                    robot_present = k;
                     break;
                 }
             }
-            if (T[i][j].target != 0)
-            {
-                if (T[i][j].left != 0)
-                {
-                    printf(red "|%2d  " stop, T[i][j].target);
+            if (grid[i][j].target != 0) {
+                if (grid[i][j].left != 0) {
+                    printf(RED "|%2d  " STOP, grid[i][j].target);
+                } else {
+                    printf(GRAY "|" STOP);
+                    printf(RED "%2d  " STOP, grid[i][j].target);
                 }
-                else
-                {
-                    printf(gray "|" stop);
-                    printf(red "%2d  " stop, T[i][j].target);
-                }
-            }
-            else if (robot_present)
-            {
-                printf(gray "| %s  " stop, ROBOT);
-            }
-            else
-            {
-                if (j == 0)
-                {
-                    printf(red "|    " stop);
-                }
-                else if (T[i][j - 1].right != 0)
-                {
-                    printf(red "|    " stop);
-                }
-                else
-                {
-                    printf(gray "|    " stop);
+            } else if (robot_present != -1) {
+                const char *emojis[] = {ROBOT, OGRE, ALIEN_MONSTER, GHOST};
+                printf(GRAY "| %s  " STOP, emojis[robot_present]);
+            } else {
+                if (j == 0) {
+                    printf(RED "|    " STOP);
+                } else if (grid[i][j - 1].right != 0) {
+                    printf(RED "|    " STOP);
+                } else {
+                    printf(GRAY "|    " STOP);
                 }
             }
         }
-        printf(red "|\n" stop);
+        printf(RED "|\n" STOP);
     }
-    for (int j = 0; j < width; j++)
-    {
-        printf(red "+----" stop);
+    for (int j = 0; j < width; j++) {
+        printf(RED "+----" STOP);
     }
-    printf(red "+\n" stop);
+    printf(RED "+\n" STOP);
 
     printf("\n");
 }
