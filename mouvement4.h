c4f294c7eb8a676992a7fe393bcd37ab2370c74c
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 14:00:58 +0200

    Update mouvement4.h

--- mouvement4.h
+++ mouvement4.h
@@ -1,9 +1,7 @@
-void right(int *x, int *y, int width, int length, struct Case T[][width])
-{
-    if (*y + 1 < width && T[*x][*y + 1].left == 0 && T[*x][*y].right == 0)
-    {
+void right(int *x, int *y, int width, int height, struct Cell grid[][width]) {
+    if (*y + 1 < width && grid[*x][*y + 1].left == 0 && grid[*x][*y].right == 0) {
         *y = *y + 1;
-        return right(x, y, width, length, T);
+        return right(x, y, width, height, grid);
     }
     printf("x = %d, y = %d\n", *x, *y);
 }
