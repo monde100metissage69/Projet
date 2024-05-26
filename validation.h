b2e76960a2d0776eda56f222664a3051a67fe238
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 22:00:31 +0200

    Update validation.h

--- validation.h
+++ validation.h
@@ -1,1 +1,11 @@
-
+int is_valid_number(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
