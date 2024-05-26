06c013699d997681ac31483d352797b937e8dfbe
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 14:29:55 +0200

    Delete mouvement3.c

--- mouvement3.c
+++
@@ -1,8 +0,0 @@
-
-void left(int *x, int *y, int height, struct Cell grid[][height]) {
-    if (*y - 1 >= 0 && grid[*x][*y - 1].right == 0 && grid[*x][*y].left == 0) {
-        *y = *y - 1;
-        return left(x, y, height, grid);
-    }
-    printf("x = %d, y = %d\n", *x, *y);
-}
