dabd2d4977b052a0075af6dcdbbb6d55a3facb40
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-05-26 22:06:15 +0200

    Update validation.h

--- validation.h
+++ validation.h
@@ -1,11 +1,8 @@
-int is_valid_number(char *str)
-{
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        if (!isdigit(str[i]))
-        {
-            return 0;
-        }
-    }
-    return 1;
-}
+#ifndef VALIDATION_H
+#define VALIDATION_H
+
+#include <stlib.h>
+
+int is_valid_number(char *str);
+
+#endif
