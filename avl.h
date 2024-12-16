aab207762d9ce724bdecf0ff09c06af8356df865
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-12-16 14:28:03 +0100

    Update and rename rotation.h to avl.h

--- avl.h
+++ avl.h
@@ -1,5 +1,5 @@
-#ifndef ROTATION_H
-#define ROTATION_H
+#ifndef AVL_H
+#define AVL_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,6 +8,11 @@
 #include <limits.h>
 #include <ctype.h>
 
+struct Station;
+struct avl;
+parbre creerAVL(int station, int capacite, long consommation);
+int max(int a, int b):
+int min(int a, int b, int c);
 parbre rotationGauche(parbre a);
 parbre rotationDroite(parbre a);
 parbre doubleRotationGauche(parbre a);
