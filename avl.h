df8d037cb6abed9af1f6052ea9bec997f4246a02
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-12-19 14:23:55 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -5,13 +5,14 @@
 #include <stdio.h>
 #include <string.h>
 
-
 struct Station;
 struct avl;
-avl *creerAVL(int s, int capacite, long consommation)
-rotationGauche(avl* a)
-rotationDroite(avl *a)
-doubleRotationGauche(avl* a)
-
+avl *creerAVL(int s, int capacite, long consommation);
+avl *rotationGauche(avl* a);
+avl *rotationDroite(avl *a);
+avl *doubleRotationGauche(avl* a);
+avl* doubleRotationDroite(avl* a);
+avl* equilibrerAVL(avl* a);
+avl* insererAVL(avl* a, int s, int capacite, long consommation, int *h);
 
 #endif
