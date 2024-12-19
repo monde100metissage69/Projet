a0c50f1ff9ed6099114adb4617bad9c808647a4e
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-12-19 14:30:10 +0100

    Update Project.c

--- Project.c
+++ Project.c
@@ -2,159 +2,211 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Définition d'un nœud AVL
-typedef struct NoeudAVL {
-    int id_station;                // Identifiant de la station
-    double capacite;               // Capacité en kWh
-    double consommation_totale;    // Somme des consommations
-    int hauteur;                   // Hauteur du nœud
-    struct NoeudAVL *gauche, *droite;
-} NoeudAVL;
+typedef struct Station {
+    int id;
+    int capacite;
+    long consommation;
+} Station;
 
-// Crée un nouveau nœud
-NoeudAVL* creerNoeud(int id, double capacite, double consommation) {
-    NoeudAVL* noeud = (NoeudAVL*)malloc(sizeof(NoeudAVL));
-    noeud->id_station = id;
-    noeud->capacite = capacite;
-    noeud->consommation_totale = consommation;
-    noeud->hauteur = 1;
-    noeud->gauche = noeud->droite = NULL;
-    return noeud;
+typedef struct avl {
+    Station s; 
+    struct avl* fg;
+    struct avl* fd;
+    int equilibre;
+} avl;
+
+avl * creerAVL(int s, int capacite, long consommation){
+    avl* nouveau = malloc(sizeof(avl));
+    if(nouveau == NULL) {
+        exit(1);
+    }
+    nouveau->s.id = s;
+    nouveau->s.capacite = capacite;
+    nouveau->s.consommation = consommation;
+    nouveau->equilibre = 0;
+    nouveau->fg = NULL;
+    nouveau->fd = NULL;
+
+    return nouveau;
 }
-
-// Retourne la hauteur d'un nœud
-int obtenirHauteur(NoeudAVL* noeud) {
-    return noeud ? noeud->hauteur : 0;
-}
-
-// Retourne le facteur d'équilibre
-int obtenirEquilibre(NoeudAVL* noeud) {
-    return noeud ? obtenirHauteur(noeud->gauche) - obtenirHauteur(noeud->droite) : 0;
-}
-
-// Effectue une rotation droite
-NoeudAVL* rotationDroite(NoeudAVL* y) {
-    NoeudAVL* x = y->gauche;
-    NoeudAVL* T2 = x->droite;
-
-    x->droite = y;
-    y->gauche = T2;
-
-    y->hauteur = 1 + (obtenirHauteur(y->gauche) > obtenirHauteur(y->droite) ? obtenirHauteur(y->gauche) : obtenirHauteur(y->droite));
-    x->hauteur = 1 + (obtenirHauteur(x->gauche) > obtenirHauteur(x->droite) ? obtenirHauteur(x->gauche) : obtenirHauteur(x->droite));
-
-    return x;
-}
-
-// Effectue une rotation gauche
-NoeudAVL* rotationGauche(NoeudAVL* x) {
-    NoeudAVL* y = x->droite;
-    NoeudAVL* T2 = y->gauche;
-
-    y->gauche = x;
-    x->droite = T2;
-
-    x->hauteur = 1 + (obtenirHauteur(x->gauche) > obtenirHauteur(x->droite) ? obtenirHauteur(x->gauche) : obtenirHauteur(x->droite));
-    y->hauteur = 1 + (obtenirHauteur(y->gauche) > obtenirHauteur(y->droite) ? obtenirHauteur(y->gauche) : obtenirHauteur(y->droite));
-
-    return y;
-}
-
-// Insère un nœud dans l'AVL
-NoeudAVL* inserer(NoeudAVL* noeud, int id, double capacite, double consommation) {
-    if (!noeud) return creerNoeud(id, capacite, consommation);
-
-    if (id < noeud->id_station)
-        noeud->gauche = inserer(noeud->gauche, id, capacite, consommation);
-    else if (id > noeud->id_station)
-        noeud->droite = inserer(noeud->droite, id, capacite, consommation);
+int max(int a, int b){
+    if(a >= b){
+        return a;
+    }
     else {
-        noeud->consommation_totale += consommation;
-        return noeud;
+        return b;
     }
+}
+int min(int a, int b, int c){
+    int min = a;
 
-    noeud->hauteur = 1 + (obtenirHauteur(noeud->gauche) > obtenirHauteur(noeud->droite) ? obtenirHauteur(noeud->gauche) : obtenirHauteur(noeud->droite));
-
-    int equilibre = obtenirEquilibre(noeud);
-
-    if (equilibre > 1 && id < noeud->gauche->id_station)
-        return rotationDroite(noeud);
-
-    if (equilibre < -1 && id > noeud->droite->id_station)
-        return rotationGauche(noeud);
-
-    if (equilibre > 1 && id > noeud->gauche->id_station) {
-        noeud->gauche = rotationGauche(noeud->gauche);
-        return rotationDroite(noeud);
+    if(b < min){
+        min = b;
     }
-
-    if (equilibre < -1 && id < noeud->droite->id_station) {
-        noeud->droite = rotationDroite(noeud->droite);
-        return rotationGauche(noeud);
+    if(c < min){
+        min = c;
     }
+    return min;
+}
+avl* rotationGauche(avl* a){
+    avl *pivot;
+    int eq_a, eq_p;
+    pivot = a->fd;
+    a->fd = pivot->fg;
+    pivot->fg = a;
 
-    return noeud;
+    eq_a = a->equilibre;
+    eq_p = pivot->equilibre;
+
+    a->equilibre = eq_a - max(eq_p, 0) -1;
+    pivot->equilibre = min(eq_a-2, eq_a + eq_p-2, eq_p-1);
+
+    a = pivot;
+    return a;
+
+}
+avl* rotationDroite(avl *a){
+    avl *pivot;
+    int eq_a, eq_p;
+    pivot = a->fg;
+    a->fg = pivot->fd;
+    pivot->fd = a;
+
+    eq_a = a->equilibre;
+    eq_p = pivot->equilibre;
+
+    a->equilibre = eq_a - max(eq_p, 0) +1; 
+    pivot->equilibre = min(eq_a+2, eq_a + eq_p+2, eq_p+1);
+
+    a = pivot;
+    return a;
+}
+avl* doubleRotationGauche(avl* a){
+    a->fd = rotationDroite(a->fd);
+    return rotationGauche(a);
 }
 
-// Parcourt l'AVL et écrit les résultats
-void ecrireResultats(NoeudAVL* racine, FILE* sortie) {
-    if (!racine) return;
-    ecrireResultats(racine->gauche, sortie);
-    fprintf(sortie, "%d;%lf;%lf\n", racine->id_station, racine->capacite, racine->consommation_totale);
-    ecrireResultats(racine->droite, sortie);
+avl* doubleRotationDroite(avl* a){
+    a->fg = rotationGauche(a->fg);
+    return rotationDroite(a);
 }
 
-// Libère la mémoire de l'AVL
-void libererArbre(NoeudAVL* racine) {
-    if (!racine) return;
-    libererArbre(racine->gauche);
-    libererArbre(racine->droite);
-    free(racine);
+
+avl* equilibrerAVL(avl* a){
+    if(a->equilibre >= 2){
+            if(a->fd->equilibre >= 0){
+            return rotationGauche(a);
+        }
+        else{
+            return doubleRotationGauche(a);
+        }
+    }
+    else if(a->equilibre <= -2){
+        if(a->fg->equilibre <= 0){
+            return rotationDroite(a);
+        }
+        else{
+            return doubleRotationDroite(a);
+        }
+    }
+    return a;
+}
+avl* insererAVL(avl* a, int s, int capacite, long consommation, int *h) {
+    if(a == NULL){
+        *h = 1;
+        return creerAVL(s, capacite, consommation);
+    }
+    else if(s < a->s.id){
+        a->fg = insererAVL(a->fg, s, capacite, consommation, h);
+    }
+    else if(s > a->s.id){
+        a->fd = insererAVL(a->fd, s, capacite, consommation, h);
+    }
+    else{
+        *h = 0; 
+        a->s.consommation += consommation;
+        a->s.capacite += capacite;
+        return a;
+    }
+    if(*h != 0){
+        a->equilibre += *h;
+        a = equilibrerAVL(a); 
+        if(a->equilibre == 0){
+            *h = 0;
+        }
+        else{
+            *h = 1;
+        }
+    }
+    return a;
+}
+void lectureFichier(const char *CSV, avl **a){
+    FILE *fichier = fopen(CSV, "r");
+    if (!fichier) {
+        exit(1);
+    }
+    char Donnee[256];
+    int h = 0;
+    fgets(Donnee, sizeof(Donnee), fichier);
+    while (fgets(Donnee, sizeof(Donnee), fichier)){
+        char *token = strtok(Donnee, ";");
+        int idstation = atoi(token);
+        token = strtok(NULL, ";");
+        int capacite;
+        if (strcmp(token, "-") != 0) {
+            capacite = atoi(token); 
+        } 
+        else {
+            capacite = 0;
+        }
+        long consommation = 0;
+        while ((token = strtok(NULL, ";")) != NULL) {
+            if (strcmp(token, "-") != 0) {
+                consommation += atol(token);
+            }
+        }
+        if (idstation > 0) {
+            *a = insererAVL(*a, idstation, capacite, consommation, &h);
+        }
+    }
+    fclose(fichier);
+}
+void ecrireInOrder(avl* a, FILE *fichier) {
+    if (a != NULL) {
+        ecrireInOrder(a->fg, fichier);
+        fprintf(fichier, "%d;%d;%ld",  "station;capacite;consommation");
+        ecrireInOrder(a->fd, fichier);
+    }
+}
+void exporter(avl* a, const char *CSV) {
+    FILE *fichier = fopen(CSV, "w");
+    if (!fichier) {
+        exit(1);
+    }
+    fprintf(fichier, "%d;%d;%ld\n", a->s.id, a->s.capacite, a->s.consommation);
+    ecrireInOrder(a, fichier);
+    fclose(fichier);
+}
+void lectureCSV(const char *chemincsv, const char *typestation, const char *typeconsommateur, const char *idcentrale){
+  avl *a = NULL;
+    lectureFichier(chemincsv, &a);
+    exporter(a, "output.csv");
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage : %s <fichier_entree> <fichier_sortie>\n", argv[0]);
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
         return 1;
     }
-
-    FILE* entree = fopen(argv[1], "r");
-    if (!entree) {
-        perror("Erreur lors de l'ouverture du fichier d'entrée");
-        return 1;
+    const char *CHEMINCSV = argv[1];
+    const char *TYPESTATION = argv[2];
+    const char *TYPECONSOMMATEUR = argv[3];
+    const char *IDCENTRALE;
+    if (argc > 4) {
+        IDCENTRALE = NULL;
     }
-
-    FILE* sortie = fopen(argv[2], "w");
-    if (!sortie) {
-        perror("Erreur lors de l'ouverture du fichier de sortie");
-        fclose(entree);
-        return 1;
+    else {
+     IDCENTRALE = argv[4];
     }
-
-    NoeudAVL* racine = NULL;
-    char ligne[1024];
-
-    fgets(ligne, sizeof(ligne), entree);
-
-    while (fgets(ligne, sizeof(ligne), entree)) {
-        int id;
-        double capacite, consommation;
-        char* token = strtok(ligne, ";");
-        token = strtok(NULL, ";");
-        id = atoi(token);
-        token = strtok(NULL, ";");
-        capacite = atof(token ? token : "0");
-        token = strtok(NULL, ";");
-        consommation = atof(token ? token : "0");
-        racine = inserer(racine, id, capacite, consommation);
-    }
-
-    fprintf(sortie, "ID Station;Capacité;Consommation Totale\n");
-    ecrireResultats(racine, sortie);
-
-    libererArbre(racine);
-    fclose(entree);
-    fclose(sortie);
-
+    lectureCSV(CHEMINCSV, TYPESTATION, TYPECONSOMMATEUR, IDCENTRALE);
     return 0;
-}
