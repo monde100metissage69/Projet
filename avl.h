59d3663be232447f5f7e07e85bb69f1ac767844e
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-12-19 14:20:56 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -4,18 +4,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h> 
-#include <limits.h>
-#include <ctype.h>
+
 
 struct Station;
 struct avl;
-parbre creerAVL(int station, int capacite, long consommation);
-int max(int a, int b):
-int min(int a, int b, int c);
-parbre rotationGauche(parbre a);
-parbre rotationDroite(parbre a);
-parbre doubleRotationGauche(parbre a);
-parbre doubleRotationDroite(parbre a);
 
 #endif
