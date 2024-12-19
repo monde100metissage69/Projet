4c9bf366d4dcae3d30692ab9490c6f8af9783b6b
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-12-19 14:25:24 +0100

    Update avl.c

--- avl.c
+++ avl.c
@@ -26,6 +26,85 @@ avl * creerAVL(int s, int capacite, long consommation){
 
     return nouveau;
 }
+int max(int a, int b){
+    if(a >= b){
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+int min(int a, int b, int c){
+    int min = a;
+
+    if(b < min){
+        min = b;
+    }
+    if(c < min){
+        min = c;
+    }
+    return min;
+}
+avl* rotationGauche(avl* a){
+    avl *pivot;
+    int eq_a, eq_p;
+    pivot = a->fd;
+    a->fd = pivot->fg;
+    pivot->fg = a;
+
+    eq_a = a->equilibre;
+    eq_p = pivot->equilibre;
+
+    a->equilibre = eq_a - max(eq_p, 0) -1;
+    pivot->equilibre = min(eq_a-2, eq_a + eq_p-2, eq_p-1);
+
+    a = pivot;
+    return a;
+
+}
+avl* rotationDroite(avl *a){
+    avl *pivot;
+    int eq_a, eq_p;
+    pivot = a->fg;
+    a->fg = pivot->fd;
+    pivot->fd = a;
+
+    eq_a = a->equilibre;
+    eq_p = pivot->equilibre;
+
+    a->equilibre = eq_a - max(eq_p, 0) +1; 
+    pivot->equilibre = min(eq_a+2, eq_a + eq_p+2, eq_p+1);
+
+    a = pivot;
+    return a;
+}
+avl* doubleRotationGauche(avl* a){
+    a->fd = rotationDroite(a->fd);
+    return rotationGauche(a);
+}
+avl* doubleRotationDroite(avl* a){
+    a->fg = rotationGauche(a->fg);
+    return rotationDroite(a);
+}
+avl* equilibrerAVL(avl* a){
+    if(a->equilibre >= 2){
+            if(a->fd->equilibre >= 0){
+            return rotationGauche(a);
+        }
+        else{
+            return doubleRotationGauche(a);
+        }
+    }
+    else if(a->equilibre <= -2){
+        if(a->fg->equilibre <= 0){
+            return rotationDroite(a);
+        }
+        else{
+            return doubleRotationDroite(a);
+        }
+    }
+    return a;
+}
 avl* insererAVL(avl* a, int s, int capacite, long consommation, int *h) {
     if(a == NULL){
         *h = 1;
@@ -55,3 +134,4 @@ avl* insererAVL(avl* a, int s, int capacite, long consommation, int *h) {
     }
     return a;
 }
+
