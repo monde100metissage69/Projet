bab4d21c66ca7a6b9a48f57b73a6285fac5c13b1
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-12-22 23:21:13 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -5,14 +5,31 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Station;
-struct avl;
-avl *creerAVL(int s, int capacite, long consommation);
-avl *rotationGauche(avl* a);
+typedef struct Station {
+    int id;
+    int capacite;
+    long consommation;
+} Station;
+typedef struct avl {
+    struct Station s;
+    struct avl *fg;
+    struct avl *fd;
+    int equilibre;
+} avl;
+avl *creerAVL(Station s, int capacite, long consommation);
+int max(int a, int b);
+int min(int a, int b, int c);
+int min2(int a, int b);
+int max2(int a, int b, int c);
+avl *rotationGauche(avl *a);
 avl *rotationDroite(avl *a);
-avl *doubleRotationGauche(avl* a);
-avl* doubleRotationDroite(avl* a);
-avl* equilibrerAVL(avl* a);
-avl* insererAVL(avl* a, int s, int capacite, long consommation, int *h);
+avl *doubleRotationGauche(avl *a);
+avl *doubleRotationDroite(avl *a);
+avl *equilibrerAVL(avl *a);
+avl *rechercherAVL(avl *a, int idstation);
+avl *insererAVL(avl *a, Station s, int capacite, long consommation, int *h);
+void traiter(avl *a, FILE *fichier);
+void parcoursPrefixe(avl *a, FILE* fichier);
+void Remplirlefichier(avl *a, FILE *fichier);
 
 #endif
