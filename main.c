89276b0dac3350a16c8c2ad834358fd81eb85443
Abderrahmane Zerargui <zerarguiab@cy-tech.fr>
2024-12-22 23:18:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,21 +4,24 @@
 #include "avl.h"
 #include "fichier.h"
 
+int main(int argc, char* argv[]){
+    const char *nomduFichier = argv[1];
+    printf("%s", argv[1]);
 
-int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        return 1;
+    const char *FINALE = argv[2];
+    avl *a = NULL;
+    int h = 0;
+
+    lectureFichier(nomduFichier, &a);
+
+    FILE* FICHIER = fopen(FINALE, "w");
+    if(FICHIER == NULL){
+        printf("Echec de l'ouverture du fichier %s :", FINALE);
+        exit(1);
     }
-    const char *CHEMINCSV = argv[1];
-    const char *TYPESTATION = argv[2];
-    const char *TYPECONSOMMATEUR = argv[3];
-    const char *IDCENTRALE;
-    if (argc > 4) {
-        IDCENTRALE = NULL;
-    }
-    else {
-     IDCENTRALE = argv[4];
-    }
-    lectureCSV(CHEMINCSV, TYPESTATION, TYPECONSOMMATEUR, IDCENTRALE);
+    Remplirlefichier(a, FICHIER);
+    fclose(FICHIER);
     return 0;
 }
+
+
